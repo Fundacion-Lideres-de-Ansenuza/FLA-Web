b28d3b95643401e72f36ad833dfab29bfe21e10c
Francisco Perez <francisco.perez@rollingcodestudio.com>
2025-07-15 01:25:36 -0300

    feat: add testimonios

---
+++ components/home/call-to-action.tsx
@@ -0,0 +1,11 @@
+export default function CallToAction() {
+  return (
+    <section className="py-10 bg-gradient-to-r from-red-600 to-red-700">
+      <div className="container mx-auto px-4">
+        <div className="rounded-full py-6 px-8 text-center shadow-lg">
+          <h2 className="text-white text-2xl font-extrabold tracking-wide">Conocé nuestra historia</h2>
+        </div>
+      </div>
+    </section>
+  )
+}
