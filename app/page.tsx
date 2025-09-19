5683e7cbbe2021cf6071f6c0b388542533efa5ce
Francisco Perez <francisco.perez@rollingcodestudio.com>
2025-09-19 19:41:35 -0300

    fix: build

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Header from "@/components/navbar"
 import Hero from "@/components/home/hero"
 import VideoSection from "@/components/home/video-section"
 import Testimonials from "@/components/home/testimonials"
 import WhyWeDoIt from "@/components/home/why-we-do-it"
 import CurrentPrograms from "@/components/home/current-programs"
-import Footer from "@/components/home/footer"
 import Stats from "@/components/stats/stats"
 
 export default function Home() {
