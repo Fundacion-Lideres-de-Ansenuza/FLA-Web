3a062ce9c253d4a01cff3ea1a7135b8d8b29adce
Francisco Perez <francisco.perez@rollingcodestudio.com>
2025-12-11 05:35:16 -0300

    feat: add upgrades

--- components/about_us/volunteers.tsx
+++ components/about_us/volunteers.tsx
@@ -12,10 +12,10 @@ export default function Volunteers(): JSX.Element {
         <h3 className="font-contrail-one text-[#160101] text-[72px] md:text-[80px] leading-tight">+100</h3>
         <h4 className="font-contrail-one text-[#160101] text-[32px] md:text-[36px] leading-tight">Voluntarios</h4>
         <h4 className="font-contrail-one text-[#160101] text-[32px] md:text-[36px] leading-tight">actuales</h4>
-      </section>
-      <section className="flex items-center justify-center">
+</section>
+    <section className="flex items-center justify-center">
         <Image src={flaMap} alt="voluntarios" width={520} height={680} className="rounded-3xl shadow-md" />
-      </section>
+    </section>
     </div>
   );
 }
